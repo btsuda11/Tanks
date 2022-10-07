2cc1d0981708681ba34db4f182e41cbb4709203b
Brendan <brendantsuda@gmail.com>
2022-10-07 13:58:14 -0700

    barrel rotates to direction of cursor

--- src/scripts/tank.js
+++ src/scripts/tank.js
@@ -5,7 +5,7 @@ export default class Tank {
     constructor(options) {
         this.barrelPos = options.pos;
         this.bodyPos = options.pos;
-        // this.angle = 0;
+        this.angle = 0;
         this.vel = [0, 0, 0, 0]; // [vx(left), vx(right), vy(up), vy(down)]
         this.game = options.game;
     }
@@ -16,7 +16,11 @@ export default class Tank {
         body.src = 'images/tanks/tankBody_blue_outline.png'
         barrel.src = 'images/tanks/tankBlue_barrel2_outline.png';
         ctx.drawImage(body, this.bodyPos[0], this.bodyPos[1]);
-        ctx.drawImage(barrel, this.barrelPos[0] + 15, this.barrelPos[1] + 15);
+        ctx.save();
+        ctx.translate(this.barrelPos[0] + 20, this.barrelPos[1] + 20);
+        ctx.rotate(this.angle - (Math.PI / 2));
+        ctx.drawImage(barrel, -5, -5);
+        ctx.restore();
     }
 
     move() {
@@ -28,7 +32,7 @@ export default class Tank {
         this.bodyPos[0] += this.vel[1];
         this.bodyPos[1] += this.vel[2];
         this.bodyPos[1] += this.vel[3];
-        // this.angle = atan2(mouseY - this.barrelPos[0], mouseX - this.barrelPos[1]);
+        this.angle = Math.atan2(this.game.cursorPos[1] - this.barrelPos[1], this.game.cursorPos[0] - this.barrelPos[0]);
     }
 
     shoot() {
