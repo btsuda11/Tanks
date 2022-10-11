c84921a78d9c7283c9c9f69f0af1320257a8ea17
Brendan <brendantsuda@gmail.com>
2022-10-10 21:09:50 -0700

    exploding tanks

--- src/scripts/mine.js
+++ src/scripts/mine.js
@@ -2,7 +2,8 @@ export default class Mine {
     constructor(pos, tank) {
         this.pos = pos;
         this.tank = tank;
-        setTimeout(() => this.tank.game.remove(this), 10000);
+        this.radius = 50;
+        setTimeout(() => this.explode(), 10000);
     }
 
     draw(ctx) {
@@ -12,5 +13,30 @@ export default class Mine {
         ctx.fill();
         ctx.strokeStyle = 'black';
         ctx.stroke();
+    }
+
+    explode() {
+        this.tank.game.tanks.forEach(tank => {
+            if (this.inRadius(tank)) {
+                tank.game.remove(this);
+                tank.game.remove(tank);
+            } else {
+                tank.game.remove(this);
+            }
+        });
+    }
+
+    inRadius(tank) {
+        let distX = this.pos[0] - tank.bodyPos[0] - (tank.width / 2);
+        let distY = this.pos[1] - tank.bodyPos[1] - (tank.height / 2);
+        if (distX > (tank.width / 2) + this.radius) return false;
+        if (distY > (tank.height / 2) + this.radius) return false;
+        if (distX <= (tank.width / 2)) {
+            return true;
+        }
+        if (distY <= (tank.height / 2)) {
+            return true;
+        }
+        return (((distX - (tank.width / 2)) ** 2) + ((distY - (tank.height / 2)) ** 2) <= this.radius ** 2);
     }
 }
