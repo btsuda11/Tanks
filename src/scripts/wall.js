e00d1f54cfb6b8065fc954b498e4f6fbd26f5e7d
Brendan <brendantsuda@gmail.com>
2022-12-06 10:33:21 -0800

    Preload wall image

--- src/scripts/wall.js
+++ src/scripts/wall.js
@@ -3,12 +3,14 @@ export default class Wall {
         this.height = height;
         this.width = width;
         this.pos = pos;
+        this.wall = new Image();
+        this.wall.src = 'assets/images/wooden_wall.png';
     }
 
     draw(ctx) {
-        let wall = new Image();
-        wall.src = 'assets/images/wooden_wall.png';
-        ctx.drawImage(wall, this.pos[0], this.pos[1], this.width, this.height);
+        // let wall = new Image();
+        // wall.src = 'assets/images/wooden_wall.png';
+        ctx.drawImage(this.wall, this.pos[0], this.pos[1], this.width, this.height);
         // ctx.fillRect(this.pos[0], this.pos[1], this.width, this.height);
     }
 }
